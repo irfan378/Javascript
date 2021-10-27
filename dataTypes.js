b0418a4030ca992809bb0be594b59ec1784199b8
irfan <irfandominate@gmail.com>
2021-10-27 08:32:24 +0530

    first commit

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+// Primitive 
+//  String
+const name='irfan';
+// Number
+const age=45;
+// Boolean
+const hasKids=true;
+// Null
+const car=null;
+// Undefined
+let test;
+// Symbol
+const sym=Symbol();
+
+// Reference Types - Objects
+// Array
+const hobbies=['cricket','coding'];
+// Object literal
+const adress={
+    city:'sringar', 
+    state:'j&k'
+}
+const today=new Date();
+console.log(typeof today);
---
+++ var_let_const.js
@@ -0,0 +1,43 @@
+// var,let,const
+// var name='irfan';
+// console.log(name);
+// var name='stupid';
+// console.log(name);
+
+// // Int var
+// var greeting;
+// console.log(greeting);
+// greeting='Hello';
+// console.log(greeting);
+
+// // Multi word vars
+// var firstName='john';//camel case
+// var first_name='steve';//Underscore
+// var FirstName='bob';//pascal case
+// var firstname;
+
+// // Let
+// let name='irfan';
+// console.log(name);
+//  name='stupid';
+// console.log(name);
+
+// const
+// const name='john';
+// console.log(name);
+// cannot reassign
+// name='irfan';
+// have to assign a value
+// const greeting;
+
+const person={
+    name:'john',
+    age:30
+}
+person.name='irfan';
+person.age=19;
+// console.log(person);
+
+const numbers=[1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
