6725a2519c987aa3770e871d7070a2f8fb58ef48
Irfan Farooq <82026975+irfan378@users.noreply.github.com>
2021-10-31 09:33:18 +0530

    Add files via upload

---
+++ windowObject.js
@@ -0,0 +1,58 @@
+// Window Methods / Objects / Properties
+// Alert
+// alert('Hello World');
+
+// Prompt
+// const input=prompt();
+// alert(input);
+
+// confirm
+// if (confirm('Are you sure')) {
+//     console.log('Yes');
+// }
+// else{
+//     console.log('No')
+// }
+
+// Outter height and width
+
+val=window.OutterHeight;
+val=window.OutterWidth;
+
+// Inner height and width
+
+val=window.InnerHeight;
+val=window.InnerWidth;
+
+// Scroll points
+val=window.scrollY;
+val=window.scrollX;
+
+// Location Object
+val=window.location;
+val=window.location.hostname;
+val=window.location.port;
+val=window.location.href;
+val=window.location.search;
+
+// Redirect
+// window.location.href=("http://google.com")
+// Reload
+// window.location.reload();
+
+// History Object
+// window.history.go();
+// val=window.history.length;
+
+// Navigator Object
+val=window.navigator;
+val=window.navigator.appName;
+val=window.navigator.appVersion;
+val=window.navigator.userAgent;
+val=window.navigator.platform;
+val=window.navigator.vendor;
+val=window.navigator.language;
+
+
+
+console.log(val);
