aca45da0015fd6ff95f61c98478c0a30ea20f6da
Irfan Farooq <irfandominate@gmail.com>
2021-11-12 10:58:49 +0530

    added news app

---
+++ arrowFunctions.js
@@ -0,0 +1,28 @@
+// Function
+// const irfan=function(){
+//     console.log('hello world');
+// }
+
+// arrow function
+// const irfan=()=>{
+//     console.log('hello world');
+// }
+// irfan();
+
+// function returns something
+// const greet=function () {
+//     return "hello world";
+// }
+// console.log(greet());
+
+// One liners do not require braces.return
+// const greet=()=>"hello world";
+// console.log(greet());
+
+// const greet=()=>({name: "irfan"});
+// console.log(greet());
+
+// const greet=name=>"good morning " + name;
+// console.log(greet('irfan'));
+
+// Single parameters do not need paranthesis but multiple parameters need paranthesis.
